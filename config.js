787c945e04720f4165fdc1eaa85125a041d7c835
yudskibiditoilet <thithuhuong861@gmail.com>
2025-09-01 13:24:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
+    titleWeb: "Gửi Lâm béo của tớ",
+    introTitle: 'Lâm yêu dấu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
